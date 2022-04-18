3015d38bffc545699fb37c49f7f2e49e7dbaefb0
Xianarchy <lzhao30@ucsc.edu>
2022-04-17 21:02:16 -0700

    6th commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,7 +15,7 @@ class Menu extends Phaser.Scene {
       
       let menuConfig = {
           fontFamily: 'Courier',
-          fontSize: '28px',
+          fontSize: '20px',
           backgroundColor: '#F3B141',
           color: '#843605',
           align: 'right',
